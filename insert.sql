dfd8219507afd47dd05824317b079d8e8fb3eba0
rss.rian <105607336+rianss12@users.noreply.github.com>
2024-09-21 11:57:42 -0300

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,5 @@
+select * From loja.cliente;
+
+insert into loja.cliente
+values (1,'Joao da Silva', 'joaosilva@gmail.com','99-99999-9999', 'Rua das Flores, 123', 'Salvador', 'Bahia', '99999-999');
+  
